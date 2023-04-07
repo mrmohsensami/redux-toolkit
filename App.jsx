ae0a52d5e7ff4e26176e9d18748c118408c72322
mr sami <107336883+mrmohsensami@users.noreply.github.com>
2023-04-07 18:50:19 +0330

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Counter from './CounterComponent';
+import PostList from './PostList';
+
+function App() {
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <h1>Redux Toolkit</h1>
+                <Counter />
+                <PostList />
+            </div>
+        </Provider>
+    );
+}
+
+export default App;
